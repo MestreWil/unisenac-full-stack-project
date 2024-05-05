530c63ba51b0c6fe49140e7c5e07c5332bc347ed
William Tavares <williamtavaresdemoura@gmail.com>
2024-05-05 12:53:42 -0300

    View metodo get album

--- backend/album/views.py
+++ backend/album/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.parsers import JSONParser
+from rest_framework import status
 
-# Create your views here.
+from .models import Album
+from .serializer import AlbumSerializer, MusicSerializer
+
+@api_view(['GET'])
+def list_albums(request):
+    albums = Album.objects.all()
+    albums_serializer = AlbumSerializer(albums, many=True)
+
+    return Response(data=albums_serializer.data, status=status.HTTP_200_OK)
+
