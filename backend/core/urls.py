3a579e35abdc03bc7fb2a08e0f70a547a8e540b0
William Tavares <williamtavaresdemoura@gmail.com>
2024-05-05 13:36:08 -0300

    inserindo as urls

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,22 +1,10 @@
-"""
-URL configuration for core project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
 from django.urls import path
 
+from .views import list_albums, get_album, create_album, create_song
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('list/', list_albums, name='list_albums'),
+    path('<int:pk>/detail/', get_album, name='get_album'),
+    path('create/', create_album, name='create_album'),
+    path('music/create/', create_song, name='create_song')
 ]
