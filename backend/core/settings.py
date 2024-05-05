747748a460a151022aa3b9d55af752067bd669a1
William Tavares <williamtavaresdemoura@gmail.com>
2024-05-05 12:25:43 -0300

    Instalando o django rest_framework

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'album'
+    'album',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
