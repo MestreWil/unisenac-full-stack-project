405161970f2f9fe26e8bd5dd2d5cb071d2a24355
oThisgo <thiagosds1701@gmail.com>
2024-07-20 00:42:21 -0300

    index working

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -2,29 +2,76 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "../styles/Home.module.css";
+import React from 'react';
 
 const inter = Inter({ subsets: ["latin"] });
 
-export default function Home({data, done}) {
+export async function getStaticProps() {
+  try {
+    const response = await fetch('http://localhost:8000/album/');
 
-  console.log('data :>> ', data);
-  console.log('done :>> ', done);
+    if (!response.ok) {
+      console.error(`HTTP error! status: ${response.status}`);
+      return {
+        props: {
+          albums: [],
+        },
+      };
+    }
 
-  return (
-    <h1>Álbuns</h1>
-  );
+    const data = await response.json();
+    console.log('Data fetched from API:', data); // Log the fetched data
+
+    return {
+      props: {
+        albums: data,
+      },
+    };
+  } catch (error) {
+    console.error('Failed to fetch albums:', error);
+
+    return {
+      props: {
+        albums: [],
+      },
+    };
+  }
 }
 
-/* export async function getStaticProps(){
+const AlbumsPage = ({ albums }) => {
+  return (
+    <div>
+      <h1>Lista de Álbuns</h1>
+      {albums.length === 0 ? (
+        <p>Nenhum álbum encontrado.</p>
+      ) : (
+        <ul>
+          {albums.map(album => (
+            <li key={album.album_name}>
+              <br />
+              <h2>{album.album_name}</h2>
+              <p><strong>Data de lançamento:</strong> {album.album_release}</p>
+              <p>
+                <strong>Músicas:</strong> <br />
+                {album.album_description.split('\r\n').map((line, index) => (
+                  <span key={index}>
+                    {line}
+                    <br />
+                  </span>
+                ))}
+              </p>
+              <p>
+                <strong>Link:</strong> <a href={album.album_link} target="_blank" rel="noopener noreferrer">{album.album_link}</a>
+              </p>
+              {album.album_image && (
+                <img src={album.album_image} alt={`Imagem do álbum ${album.album_name}`} /> 
+              )} 
+            </li> 
+          ))}
+        </ul> 
+      )}
+    </div>
+  );
+};
 
-  const response = await fetch('http://localhost:8080/album')
-
-  const data = await response.json()
-
-  return {
-    props:{
-      data: data,
-      done: true
-    }
-  }
-} */
+export default AlbumsPage;
