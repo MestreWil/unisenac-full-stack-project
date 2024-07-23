a9ba37a357a19a11906c5bf0326ee69b4aca1528
William Tavares <williamtavaresdemoura@gmail.com>
2024-07-22 22:28:20 -0300

    fix album

--- backend/album/serializers.py
+++ backend/album/serializers.py
@@ -6,7 +6,7 @@ class AlbumSerializers(serializers.ModelSerializer):
      reviews = ReviewsSerializer(many=True, read_only=True)
      class Meta:
           model = Album
-          fields = ['album_id','album_name','album_release','album_description', 'album_link', 'album_image', 'reviews']
+          fields = ['album_id','album_name','album_release','album_description', 'album_link', 'album_image', 'reviews', 'album_artist']
      
      def validate_name(self, value):
         if Artist.objects.filter(artist_name=value).exists():
